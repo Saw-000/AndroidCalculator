7c6b9e217842c8c1a87ed17bcaaa4d53bcfb5d5c
All_man <zousaaan3@gmail.com>
2019-11-27 07:21:25 +0900

    Display tapped button sequencely

--- app/src/main/java/com/all_man/androidcalculator/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/all_man/androidcalculator/calculator/CalculatorFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.all_man.androidcalculator.R
 import com.all_man.androidcalculator.databinding.FragmentCalculatorBinding
 
@@ -16,9 +18,13 @@ class CalculatorFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding : FragmentCalculatorBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_calculator, container, false)
+        val binding : FragmentCalculatorBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_calculator, container, false)
 
+        binding.viewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
+
+        // This is used so that the binding can observe LiveData updates.
+        // => automatically update the views in the layout
+        binding.lifecycleOwner = this
         return binding.root
     }
 
--- app/src/main/java/com/all_man/androidcalculator/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/all_man/androidcalculator/calculator/CalculatorViewModel.kt
@@ -1,4 +1,28 @@
 package com.all_man.androidcalculator.calculator
 
-class CalculatorViewModel {
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel : ViewModel() {
+
+    //TODO: ボタンの文字を蓄える変数
+    //private var lastCharactor : String
+
+    private val _calcText = MutableLiveData<String>()
+    val calcText : LiveData<String>
+        get() = _calcText
+
+    init{
+        _calcText.value = "0"
+    }
+
+    fun addCharactorToCalculateText(view: View) {
+        val textView = view as TextView
+        _calcText.value = calcText.value + textView.text.toString()
+    }
+
+
 }
