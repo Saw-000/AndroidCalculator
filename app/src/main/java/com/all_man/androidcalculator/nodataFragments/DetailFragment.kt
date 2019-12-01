5fe8a001bf9b6e3e1cbb4c20829d3d931bd60bfe
All_man <zousaaan3@gmail.com>
2019-12-01 22:44:42 +0900

    Add option_menu & navigation with CalculatorFragment

--- app/src/main/java/com/all_man/androidcalculator/nodataFragments/DetailFragment.kt
+++ app/src/main/java/com/all_man/androidcalculator/nodataFragments/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.all_man.androidcalculator.R
 import com.all_man.androidcalculator.databinding.FragmentDetailBinding
 
@@ -19,6 +20,10 @@ class DetailFragment : Fragment() {
 
         val binding : FragmentDetailBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_detail, container, false)
+
+        binding.backToCalculatorButton.setOnClickListener {
+            it.findNavController().navigate(DetailFragmentDirections.actionDetailFragmentToCalculatorFragment())
+        }
 
         return binding.root
     }
