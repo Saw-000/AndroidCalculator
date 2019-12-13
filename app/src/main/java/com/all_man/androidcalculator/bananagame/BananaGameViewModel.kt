34664578bcffe2e66c018638e1b9d7897de79a04
All_man <zousaaan3@gmail.com>
2019-12-13 12:12:01 +0900

    Add time-limit to the game

--- app/src/main/java/com/all_man/androidcalculator/bananagame/BananaGameViewModel.kt
+++ app/src/main/java/com/all_man/androidcalculator/bananagame/BananaGameViewModel.kt
@@ -1,9 +1,13 @@
 package com.all_man.androidcalculator.bananagame
 
 import android.app.Application
+import android.database.DatabaseUtils
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.all_man.androidcalculator.database.AppleImageDatabaseDao
 import com.all_man.androidcalculator.database.AppleImageInfo
 import kotlinx.coroutines.*
@@ -11,6 +15,27 @@ import kotlinx.coroutines.*
 class BananaGameViewModel(dataSource: AppleImageDatabaseDao,
                           application: Application,
                           tappedNum: Int): AndroidViewModel(application) {
+
+    /**Timer params*/
+    companion object {
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 7000L
+    }
+
+    // Countdown time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+    val currentTimeString = Transformations.map(currentTime) {
+        DateUtils.formatElapsedTime(it)
+    }
+    private val timer: CountDownTimer
 
     private val imageNum = tappedNum
     val database = dataSource
@@ -33,7 +58,20 @@ class BananaGameViewModel(dataSource: AppleImageDatabaseDao,
         initializeDatabase(tappedNum)
         _navigateToClearFragment.value = null
         _navigateToGameOverFragment.value = null
+
+        // timer
+        timer = object: CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(p0: Long) {
+                _currentTime.value = p0 / ONE_SECOND
+            }
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onNavigateToGameOverFragment()
+            }
+        }
+        timer.start()
     }
+
 
     // database初期化->電卓から受け取った値分のimageinfoを作成
     private fun initializeDatabase(Num: Int) {
@@ -81,21 +119,24 @@ class BananaGameViewModel(dataSource: AppleImageDatabaseDao,
     // When right-image is clicked, change _navigateToClearFragment.value to true.
     fun onNavigateToClearFragment() {
         uiScope.launch {
-            delay(1500)
+            delay(1000)
             _navigateToClearFragment.value = imageNum
         }
     }
-    // When the Navigation is finished, change _navigateToClearFragment.value to false.
+    // When the Navigation is finished, change _navigateToClearFragment.value to false
+    // for the navigation.
     fun onFinishNavigateToClearFragment() { _navigateToClearFragment.value = null }
 
-    // When wrong-image is clicked or time is up, change _navigateToGameOverFragment.value to true.
+    // When wrong-image is clicked or time is up, change _navigateToGameOverFragment.value to true
+    // for the navigation.
     fun onNavigateToGameOverFragment() {
         uiScope.launch {
-            delay(1500)
+            delay(1000)
             _navigateToGameOverFragment.value = imageNum
         }
     }
-    // When the Navigation is finished, change _navigateToGameOverFragment.value to false.
+    // When the Navigation is finished, change _navigateToGameOverFragment.value to false
+    // for the navigation.
     fun onFinishNavigateToGameOverFragment() { _navigateToGameOverFragment.value = null }
 
 
@@ -105,5 +146,6 @@ class BananaGameViewModel(dataSource: AppleImageDatabaseDao,
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
+        timer.cancel()
     }
 }
